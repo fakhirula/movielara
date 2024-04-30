2334517b553503715fe44d3f84408e4d796ce00e
Fakhirul <fakhirula27@gmail.com>
2024-04-30 16:56:12 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\MovieController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', function () {
+    return view('product');
+});
+
+Route::get('/movies', [MovieController::class, 'index']);
